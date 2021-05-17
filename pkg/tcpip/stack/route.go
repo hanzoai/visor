zhongyoub/gvisor

Handle IPv4 link local addresses in FindRoute
Test: stack_test.TestFindRouteWithForwarding
PiperOrigin-RevId: 367059519

pkg/tcpip/stack/route.go
@@ -132,7 +132,7 @@ func constructAndValidateRoute(netProto tcpip.NetworkProtocolNumber, addressEndp
 		localAddr = addressEndpoint.AddressWithPrefix().Address
 	}
 
-	if localAddressNIC != outgoingNIC && header.IsV6LinkLocalUnicastAddress(localAddr) {
+	if localAddressNIC != outgoingNIC && (header.IsV6LinkLocalUnicastAddress(localAddr) || header.IsV4LinkLocalUnicastAddress(localAddr)) {
 		addressEndpoint.DecRef()
 		return nil
 	}

pkg/tcpip/stack/stack.go
@@ -1344,7 +1344,10 @@ func (s *Stack) FindRoute(id tcpip.NICID, localAddr, remoteAddr tcpip.Address, n
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 
-	isLinkLocal := header.IsV6LinkLocalUnicastAddress(remoteAddr) || header.IsV6LinkLocalMulticastAddress(remoteAddr)
+	isLinkLocal := (header.IsV6LinkLocalUnicastAddress(remoteAddr) ||
+		header.IsV6LinkLocalMulticastAddress(remoteAddr) ||
+		header.IsV4LinkLocalUnicastAddress(remoteAddr) ||
+		header.IsV4LinkLocalMulticastAddress(remoteAddr))
 	isLocalBroadcast := remoteAddr == header.IPv4Broadcast
 	isMulticast := header.IsV4MulticastAddress(remoteAddr) || header.IsV6MulticastAddress(remoteAddr)
 	isLoopback := header.IsV4LoopbackAddress(remoteAddr) || header.IsV6LoopbackAddress(remoteAddr)
@@ -1381,7 +1384,7 @@ func (s *Stack) FindRoute(id tcpip.NICID, localAddr, remoteAddr tcpip.Address, n
 		return nil, &tcpip.ErrNetworkUnreachable{}
 	}
 
-	canForward := s.Forwarding(netProto) && !header.IsV6LinkLocalUnicastAddress(localAddr) && !isLinkLocal
+	canForward := s.Forwarding(netProto) && !header.IsV6LinkLocalUnicastAddress(localAddr) && !header.IsV4LinkLocalUnicastAddress(localAddr) && !isLinkLocal
 
 	// Find a route to the remote with the route table.
 	var chosenRoute tcpip.Route

hanzoai/visor

pkg/tcpip/stack/route.go
@@ -132,7 +132,7 @@ func constructAndValidateRoute(netProto tcpip.NetworkProtocolNumber, addressEndp
 		localAddr = addressEndpoint.AddressWithPrefix().Address
 	}
 
-	if localAddressNIC != outgoingNIC && header.IsV6LinkLocalUnicastAddress(localAddr) {
+	if localAddressNIC != outgoingNIC && (header.IsV4LinkLocalUnicastAddress(localAddr) || header.IsV6LinkLocalUnicastAddress(localAddr)) {
 		addressEndpoint.DecRef()
 		return nil
 	}

pkg/tcpip/stack/stack.go
@@ -1109,7 +1109,10 @@ func (s *Stack) FindRoute(id tcpip.NICID, localAddr, remoteAddr tcpip.Address, n
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 
-	isLinkLocal := header.IsV6LinkLocalUnicastAddress(remoteAddr) || header.IsV6LinkLocalMulticastAddress(remoteAddr)
+	isLinkLocal := (header.IsV4LinkLocalUnicastAddress(remoteAddr) ||
+		header.IsV4LinkLocalMulticastAddress(remoteAddr) ||
+		header.IsV6LinkLocalUnicastAddress(remoteAddr) ||
+		header.IsV6LinkLocalMulticastAddress(remoteAddr))
 	isLocalBroadcast := remoteAddr == header.IPv4Broadcast
 	isMulticast := header.IsV4MulticastAddress(remoteAddr) || header.IsV6MulticastAddress(remoteAddr)
 	isLoopback := header.IsV4LoopbackAddress(remoteAddr) || header.IsV6LoopbackAddress(remoteAddr)
@@ -1146,7 +1149,7 @@ func (s *Stack) FindRoute(id tcpip.NICID, localAddr, remoteAddr tcpip.Address, n
 		return nil, &tcpip.ErrNetworkUnreachable{}
 	}
 
-	onlyGlobalAddresses := !header.IsV6LinkLocalUnicastAddress(localAddr) && !isLinkLocal
+	onlyGlobalAddresses := !isLinkLocal && !header.IsV4LinkLocalUnicastAddress(localAddr) && !header.IsV6LinkLocalUnicastAddress(localAddr)
 
 	// Find a route to the remote with the route table.
 	var chosenRoute tcpip.Route