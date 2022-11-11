Code-Hex/gvisor

IO_URING feature gate.
Feature gate for the IO_URING until it is more substantially implemented.

PiperOrigin-RevId: 479354100

pkg/sentry/syscalls/linux/vfs2/iouringfs.go
@@ -25,6 +25,10 @@ import (
 
 // IOUringSetup implements linux syscall io_uring_setup(2).
 func IOUringSetup(t *kernel.Task, args arch.SyscallArguments) (uintptr, *kernel.SyscallControl, error) {
+	if !kernel.IOUringFSEnabled {
+		return 0, nil, linuxerr.EOPNOTSUPP
+	}
+
 	entries := uint32(args[0].Uint())
 	paramsAddr := args[1].Pointer()
 	var params linux.IOUringParams
@@ -77,6 +81,10 @@ func IOUringSetup(t *kernel.Task, args arch.SyscallArguments) (uintptr, *kernel.
 
 // IOUringEnter implements linux syscall io_uring_enter(2).
 func IOUringEnter(t *kernel.Task, args arch.SyscallArguments) (uintptr, *kernel.SyscallControl, error) {
+	if !kernel.IOUringFSEnabled {
+		return 0, nil, linuxerr.EOPNOTSUPP
+	}
+
 	fd := int32(args[0].Int())
 	toSubmit := uint32(args[1].Uint())
 	minComplete := uint32(args[2].Uint())

runsc/boot/loader.go
@@ -241,6 +241,7 @@ func New(args Args) (*Loader, error) {
 	kernel.VFS2Enabled = true
 	kernel.FUSEEnabled = args.Conf.FUSE
 	kernel.LISAFSEnabled = args.Conf.Lisafs
+	kernel.IOUringFSEnabled = args.Conf.IOUringFS
 	bufferv2.PoolingEnabled = args.Conf.BufferPooling
 	vfs2.Override()
 

runsc/cli/main.go
@@ -226,6 +226,7 @@ func Main(version string) {
 	log.Infof("\t\tFileAccess: %v, overlay: %t", conf.FileAccess, conf.Overlay)
 	log.Infof("\t\tNetwork: %v, logging: %t", conf.Network, conf.LogPackets)
 	log.Infof("\t\tStrace: %t, max size: %d, syscalls: %s", conf.Strace, conf.StraceLogSize, conf.StraceSyscalls)
+	log.Infof("\t\tIOURINGFS: %t", conf.IOUringFS)
 	log.Infof("\t\tLISAFS: %t", conf.Lisafs)
 	log.Infof("\t\tDebug: %v", conf.Debug)
 	log.Infof("\t\tSystemd: %v", conf.SystemdCgroup)

runsc/config/config.go
@@ -252,6 +252,10 @@ type Config struct {
 	// used.
 	DCache int `flag:"dcache"`
 
+	// IOUringFS enables support for the io_uring API calls to perform
+	// asynchronous I/O operations.
+	IOUringFS bool `flag:"iouringfs"`
+
 	// TestOnlyAllowRunAsCurrentUserWithoutChroot should only be used in
 	// tests. It allows runsc to start the sandbox process as the current
 	// user, and without chrooting the sandbox process. This can be

runsc/config/flags.go
@@ -90,6 +90,7 @@ func RegisterFlags(flagSet *flag.FlagSet) {
 	flagSet.Bool("ignore-cgroups", false, "don't configure cgroups.")
 	flagSet.Int("fdlimit", -1, "Specifies a limit on the number of host file descriptors that can be open. Applies separately to the sentry and gofer. Note: each file in the sandbox holds more than one host FD open.")
 	flagSet.Int("dcache", -1, "Set the global dentry cache size. This acts as a coarse-grained control on the number of host FDs simultaneously open by the sentry. If negative, per-mount caches are used.")
+	flagSet.Bool("iouringfs", false, "TEST ONLY; Enables io_uring API for asynchronous I/O.")
 
 	// Flags that control sandbox runtime behavior: network related.
 	flagSet.Var(networkTypePtr(NetworkSandbox), "network", "specifies which network to use: sandbox (default), host, none. Using network inside the sandbox is more secure because it's isolated from the host network.")

hanzoai/visor

pkg/sentry/syscalls/linux/vfs2/iouringfs.go
@@ -25,6 +25,10 @@ import (
 
 // IOUringSetup implements linux syscall io_uring_setup(2).
 func IOUringSetup(t *kernel.Task, args arch.SyscallArguments) (uintptr, *kernel.SyscallControl, error) {
+	if !kernel.IOUringEnabled {
+		return 0, nil, linuxerr.ENOSYS
+	}
+
 	entries := uint32(args[0].Uint())
 	paramsAddr := args[1].Pointer()
 	var params linux.IOUringParams
@@ -77,6 +81,10 @@ func IOUringSetup(t *kernel.Task, args arch.SyscallArguments) (uintptr, *kernel.
 
 // IOUringEnter implements linux syscall io_uring_enter(2).
 func IOUringEnter(t *kernel.Task, args arch.SyscallArguments) (uintptr, *kernel.SyscallControl, error) {
+	if !kernel.IOUringEnabled {
+		return 0, nil, linuxerr.ENOSYS
+	}
+
 	fd := int32(args[0].Int())
 	toSubmit := uint32(args[1].Uint())
 	minComplete := uint32(args[2].Uint())

runsc/boot/loader.go
@@ -239,6 +239,7 @@ func New(args Args) (*Loader, error) {
 
 	kernel.FUSEEnabled = args.Conf.FUSE
 	kernel.LISAFSEnabled = args.Conf.Lisafs
+	kernel.IOUringEnabled = args.Conf.IOUring
 	vfs2.Override()
 
 	// Make host FDs stable between invocations. Host FDs must map to the exact

runsc/cli/main.go
@@ -226,6 +226,7 @@ func Main(version string) {
 	log.Infof("\t\tFileAccess: %v, overlay: %t", conf.FileAccess, conf.Overlay)
 	log.Infof("\t\tNetwork: %v, logging: %t", conf.Network, conf.LogPackets)
 	log.Infof("\t\tStrace: %t, max size: %d, syscalls: %s", conf.Strace, conf.StraceLogSize, conf.StraceSyscalls)
+	log.Infof("\t\tIOURING: %t", conf.IOUring)
 	log.Infof("\t\tLISAFS: %t", conf.Lisafs)
 	log.Infof("\t\tDebug: %v", conf.Debug)
 	log.Infof("\t\tSystemd: %v", conf.SystemdCgroup)

runsc/config/config.go
@@ -257,6 +257,10 @@ type Config struct {
 	// used.
 	DCache int `flag:"dcache"`
 
+	// IOUring enables support for the IO_URING API calls to perform
+	// asynchronous I/O operations.
+	IOUring bool `flag:"iouring"`
+
 	// TestOnlyAllowRunAsCurrentUserWithoutChroot should only be used in
 	// tests. It allows runsc to start the sandbox process as the current
 	// user, and without chrooting the sandbox process. This can be

runsc/config/flags.go
@@ -90,6 +90,7 @@ func RegisterFlags(flagSet *flag.FlagSet) {
 	flagSet.Bool("ignore-cgroups", false, "don't configure cgroups.")
 	flagSet.Int("fdlimit", -1, "Specifies a limit on the number of host file descriptors that can be open. Applies separately to the sentry and gofer. Note: each file in the sandbox holds more than one host FD open.")
 	flagSet.Int("dcache", -1, "Set the global dentry cache size. This acts as a coarse-grained control on the number of host FDs simultaneously open by the sentry. If negative, per-mount caches are used.")
+	flagSet.Bool("iouring", false, "TEST ONLY; Enables IO_URING API for asynchronous I/O.")
 
 	// Flags that control sandbox runtime behavior: network related.
 	flagSet.Var(networkTypePtr(NetworkSandbox), "network", "specifies which network to use: sandbox (default), host, none. Using network inside the sandbox is more secure because it's isolated from the host network.")