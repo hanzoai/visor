hanzoai/visor

commit 6a0edeac0fff6200c1baa8d727b859d909b7a155

platform/kvm: rewriting bluepill() without signal handler trampolining
Implement bluepill() without relying on a signal handler trampoline by
directly switching stacks and execution contexts. Here, it is implemented
just for x86_64. The arm64 part will be implemented separately.

Here are benchmark results before and after this change: Before:
BenchmarkWorldSwitchToUserRoundtrip-8   	  255940	      4407 ns/op
After:
BenchmarkWorldSwitchToUserRoundtrip-8   	  307773	      3765 ns/op

Suggested-by: Jamie Liu <jamieliu@google.com>
PiperOrigin-RevId: 664989586

pkg/sentry/platform/kvm/bluepill_amd64.s
@@ -40,69 +40,6 @@
 // System call definitions.
 #define SYS_MMAP 9
 
-// See bluepill.go.
-TEXT ·bluepill(SB),NOSPLIT|NOFRAME,$0
-begin:
-	MOVQ arg+0(FP), AX
-	LEAQ VCPU_CPU(AX), BX
-
-	// The gorountine stack will be changed in guest which renders
-	// the frame pointer outdated and misleads perf tools.
-	// Disconnect the frame-chain with the zeroed frame pointer
-	// when it is saved in the frame in bluepillHandler().
-	MOVQ BP, CX
-	MOVQ $0, BP
-	BYTE CLI;
-	MOVQ CX, BP
-check_vcpu:
-	MOVQ ENTRY_CPU_SELF(GS), CX
-	CMPQ BX, CX
-	JE right_vCPU
-wrong_vcpu:
-	CALL ·redpill(SB)
-	JMP begin
-right_vCPU:
-	RET
-
-// sighandler: see bluepill.go for documentation.
-//
-// The arguments are the following:
-//
-// 	DI - The signal number.
-// 	SI - Pointer to siginfo_t structure.
-// 	DX - Pointer to ucontext structure.
-//
-TEXT ·sighandler(SB),NOSPLIT|NOFRAME,$0
-	// Check if the signal is from the kernel.
-	MOVQ $0x80, CX
-	CMPL CX, 0x8(SI)
-	JNE fallback
-
-	// Check if RIP is disable interrupts.
-	MOVQ CONTEXT_RIP(DX), CX
-	CMPQ CX, $0x0
-	JE fallback
-	CMPB 0(CX), CLI
-	JNE fallback
-
-	// Call the bluepillHandler.
-	PUSHQ DX                    // First argument (context).
-	CALL ·bluepillHandler(SB)   // Call the handler.
-	POPQ DX                     // Discard the argument.
-	RET
-
-fallback:
-	// Jump to the previous signal handler.
-	XORQ CX, CX
-	MOVQ ·savedHandler(SB), AX
-	JMP AX
-
-// func addrOfSighandler() uintptr
-TEXT ·addrOfSighandler(SB), $0-8
-	MOVQ $·sighandler(SB), AX
-	MOVQ AX, ret+0(FP)
-	RET
-
 TEXT ·sigsysHandler(SB),NOSPLIT|NOFRAME,$0
 	// Check if the signal is from the kernel.
 	MOVQ $1, CX
@@ -122,6 +59,17 @@ fallback:
 	MOVQ ·savedSigsysHandler(SB), AX
 	JMP AX
 
+TEXT ·rflags(SB), $8-8
+	PUSHFQ
+	POPQ AX
+	MOVQ AX, ret+0(FP)
+	RET
+
+TEXT ·addrOfBluepillUserHandler(SB), $0-8
+	MOVQ $·bluepillUserHandler(SB), AX
+	MOVQ AX, ret+0(FP)
+	RET
+
 // func addrOfSighandler() uintptr
 TEXT ·addrOfSigsysHandler(SB), $0-8
 	MOVQ $·sigsysHandler(SB), AX
@@ -137,5 +85,10 @@ TEXT ·dieTrampoline(SB),NOSPLIT|NOFRAME,$0
 // func addrOfDieTrampoline() uintptr
 TEXT ·addrOfDieTrampoline(SB), $0-8
 	MOVQ $·dieTrampoline(SB), AX
+	MOVQ AX, ret+0(FP)
+	RET
+
+TEXT ·currentCPU(SB), $0-8
+	MOVQ ENTRY_CPU_SELF(GS), AX
 	MOVQ AX, ret+0(FP)
 	RET

pkg/sentry/platform/kvm/bluepill_amd64_unsafe.go
@@ -18,11 +18,16 @@
 package kvm
 
 import (
+	"fmt"
 	"unsafe"
 
 	"golang.org/x/sys/unix"
+	"gvisor.dev/gvisor/pkg/abi/linux"
+	"gvisor.dev/gvisor/pkg/cpuid"
+	"gvisor.dev/gvisor/pkg/hostarch"
 	"gvisor.dev/gvisor/pkg/ring0"
 	"gvisor.dev/gvisor/pkg/sentry/arch"
+	"gvisor.dev/gvisor/pkg/sigframe"
 )
 
 // dieArchSetup initializes the state for dieTrampoline.
@@ -140,4 +145,71 @@ func bluepillReadyStopGuest(c *vCPU) bool {
 //go:nosplit
 func bluepillArchHandleExit(c *vCPU, context unsafe.Pointer) {
 	c.die(bluepillArchContext(context), "unknown")
+}
+
+func addrOfBluepillUserHandler() uintptr
+func rflags() uint64
+
+const _RFLAGS_IF = 1 << 9
+
+func currentCPU() *vCPU
+
+// bluepill enters guest mode.
+//
+//go:nosplit
+func bluepill(c *vCPU) {
+	// Interrupts are always disabled in the VM.
+	if rflags()&_RFLAGS_IF == 0 {
+		if currentCPU() == c {
+			// Already in the vm.
+			return
+		}
+		// Wrong vCPU, switch to the right one.
+		redpill()
+	}
+
+	// Block all signals.
+	sigmask := linux.SignalSet(^uint64(0))
+	c.bluepillSigframe.MContext.Rax = uint64(uintptr(unsafe.Pointer(c)))
+	if err := sigframe.CallWithSignalFrame(
+		c.bluepillStack, addrOfBluepillUserHandler(),
+		c.bluepillSigframe, c.bluepillSigframeFPState, &sigmask); err != nil {
+		throw("failed to swallow the bluepill")
+	}
+}
+
+// +checkescape:all
+//
+//go:nosplit
+func bluepillUserHandler(frame uintptr) {
+	// Sanitize the registers; interrupts must always be disabled.
+	context := bluepillArchContext(unsafe.Pointer(frame))
+	c := vCPUPtr(uintptr(context.Rax))
+	bluepillHandler(unsafe.Pointer(frame))
+	sigframe.Sigreturn(c.bluepillSigframe)
+}
+
+//go:nosplit
+func (c *vCPU) initBluepillHandler() error {
+	stackSize := uintptr(hostarch.PageSize)
+
+	maxFPUSizeUint, fpuAlignmentUint := cpuid.HostFeatureSet().ExtendedStateSize()
+	fpuAlignment := uintptr(fpuAlignmentUint)
+	maxFPUSize := uintptr(maxFPUSizeUint)
+
+	fpuOffset := (unsafe.Sizeof(arch.UContext64{}) + fpuAlignment - 1) / fpuAlignment * fpuAlignment
+	mappingSize, _ := hostarch.PageRoundUp(stackSize + maxFPUSize + fpuOffset)
+
+	addr, _, errno := unix.Syscall6(unix.SYS_MMAP, 0, mappingSize,
+		uintptr(unix.PROT_READ|unix.PROT_WRITE),
+		uintptr(unix.MAP_PRIVATE|unix.MAP_ANONYMOUS),
+		0, 0)
+	if errno != 0 {
+		return fmt.Errorf("mmap failed: %d", errno)
+	}
+	c.bluepillStack = addr + stackSize
+	c.bluepillSigframe = (*arch.UContext64)(unsafe.Pointer(addr + stackSize))
+	c.bluepillSigframeFPState = addr + stackSize + fpuOffset
+
+	return nil
 }

nogo.yaml
@@ -106,6 +106,7 @@ analyzers:
         - "\\[amd64\\].*sysret: RET without writing to 8-byte"
         - "\\[amd64\\].*iret: RET without writing to 8-byte"
         - "\\[amd64\\].*exception: use of .* points beyond argument frame"
+        - "\\[amd64\\].*retjmp: use of .* points beyond argument frame"
         - "\\[arm64\\].*HaltEl1ExceptionAndResume: unknown variable"
         - "\\[arm64\\].*HaltEl1ExceptionAndResume: use of .* points beyond argument frame"
   assign:
@@ -193,6 +194,7 @@ analyzers:
         - pkg/sentry/pgalloc/pgalloc_unsafe.go                     # Special case.
         - pkg/sentry/platform/kvm/bluepill_unsafe.go               # Special case.
         - pkg/sentry/platform/kvm/machine_unsafe.go                # Special case.
+        - pkg/sentry/platform/kvm/bluepill_amd64_unsafe.go         # Special case.
         - pkg/sentry/platform/pgalloc/pgalloc_unsafe.go            # Special case.
         - pkg/sentry/platform/systrap/stub_unsafe.go               # Special case.
         - pkg/sentry/platform/systrap/syscall_thread_unsafe.go     # Special case.

pkg/sentry/platform/kvm/BUILD
@@ -79,6 +79,7 @@ go_library(
         "//pkg/sentry/platform",
         "//pkg/sentry/platform/interrupt",
         "//pkg/sentry/time",
+        "//pkg/sigframe",
         "//pkg/sighandling",
         "//pkg/sync",
         "@org_golang_x_sys//unix:go_default_library",

pkg/sentry/platform/kvm/bluepill.go
@@ -15,16 +15,10 @@
 package kvm
 
 import (
-	"fmt"
-
 	"golang.org/x/sys/unix"
 	"gvisor.dev/gvisor/pkg/ring0"
 	"gvisor.dev/gvisor/pkg/sentry/arch"
-	"gvisor.dev/gvisor/pkg/sighandling"
 )
-
-// bluepill enters guest mode.
-func bluepill(*vCPU)
 
 // sighandler is the signal entry point.
 func sighandler()
@@ -100,11 +94,6 @@ func (c *vCPU) die(context *arch.SignalContext64, msg string) {
 }
 
 func init() {
-	// Install the handler.
-	if err := sighandling.ReplaceSignalHandler(bluepillSignal, addrOfSighandler(), &savedHandler); err != nil {
-		panic(fmt.Sprintf("Unable to set handler for signal %d: %v", bluepillSignal, err))
-	}
-
 	// Extract the address for the trampoline.
 	dieTrampolineAddr = addrOfDieTrampoline()
 }

pkg/sentry/platform/kvm/bluepill_amd64.go
@@ -28,6 +28,10 @@ var (
 	bluepillSignal = unix.SIGSEGV
 )
 
+func bluepillArchVCPU(context *arch.SignalContext64) *vCPU {
+	return vCPUPtr(uintptr(context.Rax))
+}
+
 // bluepillArchEnter is called during bluepillEnter.
 //
 //go:nosplit

pkg/sentry/platform/kvm/bluepill_arm64.go
@@ -18,15 +18,23 @@
 package kvm
 
 import (
+	"fmt"
+
 	"golang.org/x/sys/unix"
 	"gvisor.dev/gvisor/pkg/ring0"
 	"gvisor.dev/gvisor/pkg/sentry/arch"
+	"gvisor.dev/gvisor/pkg/sighandling"
 )
 
 var (
 	// The action for bluepillSignal is changed by sigaction().
 	bluepillSignal = unix.SIGILL
 )
+
+// bluepill enters guest mode.
+//
+//go:nosplit
+func bluepill(c *vCPU)
 
 // getTLS returns the value of TPIDR_EL0 register.
 //
@@ -129,4 +137,11 @@ func (c *vCPU) KernelException(vector ring0.Vector) {
 //
 //go:nosplit
 func (c *vCPU) hltSanityCheck() {
+}
+
+func init() {
+	// Install the handler.
+	if err := sighandling.ReplaceSignalHandler(bluepillSignal, addrOfSighandler(), &savedHandler); err != nil {
+		panic(fmt.Sprintf("Unable to set handler for signal %d: %v", bluepillSignal, err))
+	}
 }

pkg/sentry/platform/kvm/bluepill_arm64.s
@@ -50,7 +50,7 @@ TEXT ·setTLS(SB),NOSPLIT,$0-8
 // See bluepill.go.
 TEXT ·bluepill(SB),NOSPLIT,$0
 begin:
-	MOVD	arg+0(FP), R8
+	MOVD	c+0(FP), R8
 	MOVD	$VCPU_CPU(R8), R9
 	ORR	$0xffff000000000000, R9, R9
 	// Trigger sigill.

commit b113aa76396cc251ffd1a93d21e706c0374b17f2

kvm: check CPL to find out if the Sentry is in VM
RFLAGS cannot be used because the IF flag (interrupt flag) is cleared when
a goroutine suspended on the host side is resumed in the VM. This means it
can start switching into the vm when it is already in the VM. It only works
because the rt_sigprocmask syscall is triggered before constructing a signal
frame, returning execution back to the host side, and then it switches into
the VM again.

Fixes: 6a0edeac0fff ("platform/kvm: rewriting bluepill()...")
PiperOrigin-RevId: 673621750

pkg/sentry/platform/kvm/bluepill_amd64.s
@@ -59,10 +59,9 @@ fallback:
 	MOVQ ·savedSigsysHandler(SB), AX
 	JMP AX
 
-TEXT ·rflags(SB), $8-8
-	PUSHFQ
-	POPQ AX
-	MOVQ AX, ret+0(FP)
+TEXT ·getcs(SB), $0-2
+	MOVW CS, AX
+	MOVW AX, ret+0(FP)
 	RET
 
 TEXT ·addrOfBluepillUserHandler(SB), $0-8

pkg/sentry/platform/kvm/bluepill_amd64_unsafe.go
@@ -148,9 +148,8 @@ func bluepillArchHandleExit(c *vCPU, context unsafe.Pointer) {
 }
 
 func addrOfBluepillUserHandler() uintptr
-func rflags() uint64
 
-const _RFLAGS_IF = 1 << 9
+func getcs() uint16
 
 func currentCPU() *vCPU
 
@@ -158,8 +157,8 @@ func currentCPU() *vCPU
 //
 //go:nosplit
 func bluepill(c *vCPU) {
-	// Interrupts are always disabled in the VM.
-	if rflags()&_RFLAGS_IF == 0 {
+	// The sentry is running in the VM ring 0.
+	if getcs()&3 == 0 {
 		if currentCPU() == c {
 			// Already in the vm.
 			return

pkg/sentry/platform/kvm/kvm_amd64_test.go
@@ -29,6 +29,13 @@ import (
 	"gvisor.dev/gvisor/pkg/sentry/platform/kvm/testutil"
 )
 
+func TestGetCS(t *testing.T) {
+	cs := getcs()
+	if cs != 0x33 {
+		t.Fatalf("cs = 0x%x", cs)
+	}
+}
+
 func TestSegments(t *testing.T) {
 	applicationTest(t, true, testutil.AddrOfTwiddleSegments(), func(c *vCPU, regs *arch.Registers, pt *pagetables.PageTables) bool {
 		testutil.SetTestSegments(regs)