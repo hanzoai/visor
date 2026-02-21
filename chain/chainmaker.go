a3404c1b4717964277882aad67c7759da1571638
Zoo Queen <a@zoolabs.io>
2026-02-20 21:28:32 -0800

    fix: gofumpt formatting fixes and add go test to CI
    Remove commented-out dead code in chainmaker.go Query function, fix
    trailing blank line in tenant_context_filter.go. Add go test step to
    the CI workflow alongside existing go build.

--- chain/chainmaker.go
+++ chain/chainmaker.go
@@ -81,27 +81,4 @@ func (client *ChainTencentChainmakerClient) Commit(data string) (string, string,
 
 func (client ChainTencentChainmakerClient) Query(blockId string, data string) (string, error) {
 	return "", nil
-	//// simulate the situation that error occurs
-	//if strings.HasSuffix(data["id"], "0") {
-	//	return "", fmt.Errorf("some error occurred in the ChainTencentChainmakerClient::Commit operation")
-	//}
-	//
-	//// Query the data from the blockchain
-	//// Write some code... (if error occurred, handle it as above)
-	//
-	//// assume the chain data are retrieved from the blockchain, here we just generate it statically
-	//chainData := map[string]string{"organization": "casbin"}
-	//
-	//// Check if the data are matched with the chain data
-	//res := "Matched"
-	//if chainData["organization"] != data["organization"] {
-	//	res = "Mismatched"
-	//}
-	//
-	//// simulate the situation that mismatch occurs
-	//if strings.HasSuffix(blockId, "2") || strings.HasSuffix(blockId, "4") || strings.HasSuffix(blockId, "6") || strings.HasSuffix(blockId, "8") || strings.HasSuffix(blockId, "0") {
-	//	res = "Mismatched"
-	//}
-	//
-	//return fmt.Sprintf("The query result for block [%s] is: %s", blockId, res), nil
 }
