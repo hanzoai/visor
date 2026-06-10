hanzoai/visor

commit 5d1cd10493740ca2e15ab70133b3acf4f0e18d3d

nftables: Added nat expression support.
PiperOrigin-RevId: 926479213

pkg/tcpip/nftables/nft_nat.go
@@ -1,0 +1,253 @@
+// Copyright 2025 The gVisor Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package nftables
+
+import (
+	"encoding/binary"
+
+	"gvisor.dev/gvisor/pkg/abi/linux"
+	"gvisor.dev/gvisor/pkg/log"
+	"gvisor.dev/gvisor/pkg/syserr"
+
+	"gvisor.dev/gvisor/pkg/tcpip"
+	"gvisor.dev/gvisor/pkg/tcpip/header"
+	"gvisor.dev/gvisor/pkg/tcpip/stack"
+)
+
+// natOp represents the NFT_NAT expression.
+type natOp struct {
+	// sregAddrMinIdx is the starting index in the register set to read/write address min data.
+	sregAddrMinIdx int8
+	// sregAddrMaxIdx is the starting index in the register set to read/write address max data.
+	sregAddrMaxIdx int8
+	// sregProtoMinIdx is the starting index in the register set to read/write proto min data.
+	sregProtoMinIdx int8
+	// sregProtoMaxIdx is the starting index in the register set to read/write proto max data.
+	sregProtoMaxIdx int8
+	// manipType is the type of NAT operation, either SNAT or DNAT.
+	manipType stack.NATType
+	// family is the Address family of the NAT operation, either IPv4 or IPv6.
+	family uint8
+	// flags is the flags for the NAT operation.
+	flags uint16
+}
+
+// regIdxOrDefault returns the register index if it is valid
+// otherwise returns the default value.
+func regIdxOrDefault(reg int8, len int, defaultIdx int8) int8 {
+	if reg < 0 {
+		return defaultIdx
+	}
+	v, err := regNumToIdx(uint8(reg), len)
+	if err == nil {
+		return int8(v)
+	}
+	return defaultIdx
+}
+
+// newNATOp creates a new NAT operation.
+func newNATOp(nt, family uint8, sregAddrMin, sregAddrMax, sregProtoMin, sregProtoMax int8, flags uint16) (*natOp, *syserr.AnnotatedError) {
+	natOp := &natOp{
+		family: family,
+		flags:  flags,
+	}
+	natOp.manipType = stack.ToNATType(nt)
+	if natOp.manipType == stack.NATUnknown {
+		return nil, syserr.NewAnnotatedError(syserr.ErrInvalidArgument, "Nftables: Invalid NAT type")
+	}
+	len := header.IPv4AddressSize
+	if family == linux.NFPROTO_IPV6 {
+		len = header.IPv6AddressSize
+	}
+	natOp.sregAddrMinIdx = regIdxOrDefault(sregAddrMin, len, -1 /*defaultIdx*/)
+	natOp.sregAddrMaxIdx = regIdxOrDefault(sregAddrMax, len, natOp.sregAddrMinIdx)
+	natOp.sregProtoMinIdx = regIdxOrDefault(sregProtoMin, 2 /*len*/, -1)
+	natOp.sregProtoMaxIdx = regIdxOrDefault(sregProtoMax, 2 /*len*/, natOp.sregProtoMinIdx)
+	return natOp, nil
+}
+
+// nfNatRange is the equivalent of struct nf_nat_range2 in Linux.
+type nfNatRange struct {
+	minAddr  tcpip.Address
+	maxAddr  tcpip.Address
+	minProto uint16
+	maxProto uint16
+	flags    uint16
+}
+
+// setupAddr returns the min and max addresses from the register set.
+func (n *natOp) getAddrRange(regs *registerSet) (minAddr, maxAddr tcpip.Address) {
+	regBuffer := regs.data
+	sz := header.IPv4AddressSize
+	if n.family == linux.NFPROTO_IPV6 {
+		sz = header.IPv6AddressSize
+	}
+	minAddr = tcpip.AddrFromSlice(regBuffer[n.sregAddrMinIdx : int(n.sregAddrMinIdx)+sz])
+	maxAddr = tcpip.AddrFromSlice(regBuffer[n.sregAddrMaxIdx : int(n.sregAddrMaxIdx)+sz])
+	return
+}
+
+// getProtoRange returns the min max proto data.
+// For example, for tcp/udp this proto data is the port range.
+// See `nf_conntrack_man_proto` in kernel.
+func (n *natOp) getProtoRange(regs *registerSet) (minProto, maxProto uint32) {
+	regBuffer := regs.data
+	minBuf := regBuffer[n.sregProtoMinIdx : n.sregProtoMinIdx+2]
+	minProto = uint32(minBuf[0])<<8 | uint32(minBuf[1])
+	maxBuf := regBuffer[n.sregProtoMaxIdx : n.sregProtoMaxIdx+2]
+	maxProto = uint32(maxBuf[0])<<8 | uint32(maxBuf[1])
+	return
+}
+
+// setupNetmap sets up the netmap for the NAT operation.
+// See `nft_nat_setup_netmap` in kernel.
+// TODO: b/486197011 - Support and verify netmaps.
+func (n *natOp) setupNetmap(pkt *stack.PacketBuffer, minAddr, maxAddr *tcpip.Address) {
+	var manipAddr tcpip.Address
+	var addrLen int
+
+	switch n.manipType {
+	case stack.SNAT:
+		manipAddr = pkt.Network().SourceAddress()
+	case stack.DNAT:
+		manipAddr = pkt.Network().DestinationAddress()
+	}
+
+	addrLen = header.IPv4AddressSize
+	if pkt.NetworkProtocolNumber == header.IPv6ProtocolNumber {
+		addrLen = header.IPv6AddressSize
+	}
+
+	manipAddrSlice := manipAddr.AsSlice()
+	minSlice := minAddr.AsSlice()
+	maxSlice := maxAddr.AsSlice()
+	newAddrBytes := make([]byte, addrLen)
+	words := addrLen / 4 // size of uint32
+	for i := 0; i < words; i++ {
+		offset := i * 4 // offset in bytes
+		m := binary.BigEndian.Uint32(manipAddrSlice[offset:])
+		min := binary.BigEndian.Uint32(minSlice[offset:])
+		max := binary.BigEndian.Uint32(maxSlice[offset:])
+
+		netmask := ^(min ^ max)
+		m &= ^netmask
+		m |= min & netmask
+		binary.BigEndian.PutUint32(newAddrBytes[offset:], m)
+	}
+
+	*minAddr = tcpip.AddrFromSlice(newAddrBytes)
+	*maxAddr = *minAddr
+}
+
+// evaluate performs NAT setup on the connection.
+// Called when the packet matches the NAT op configured.
+func (n *natOp) evaluate(regs *registerSet, pkt *stack.PacketBuffer, rule *Rule) {
+	// Just fill the data for the NAT operation.
+	changeAddress := false
+	changePort := false
+	var minAddr, maxAddr tcpip.Address
+	if n.sregAddrMinIdx >= 0 {
+		changeAddress = true
+		minAddr, maxAddr = n.getAddrRange(regs)
+		if (n.flags & linux.NF_NAT_RANGE_MAP_IPS) != 0 {
+			n.setupNetmap(pkt, &minAddr, &maxAddr)
+		}
+	}
+
+	var minProto, maxProto uint32
+	if n.sregProtoMinIdx >= 0 {
+		changePort = true
+		minProto, maxProto = n.getProtoRange(regs)
+	}
+
+	// TODO: b/486197011 - Implement NAT operation.
+	log.Warningf("Nftables: natOp.evaluate() is not implemented, changeAddress=%t, changePort=%t, minAddr=%s, maxAddr=%s, minProto=%d, maxProto=%d", changeAddress, changePort, minAddr, maxAddr, minProto, maxProto)
+}
+
+func (n *natOp) GetExprName() string {
+	return OpTypeNAT.String()
+}
+
+func (n *natOp) Dump() ([]byte, *syserr.AnnotatedError) {
+	log.Warningf("Nftables: natOp.Dump() is not implemented")
+	return nil, nil
+}
+
+var natAttrPolicy = []NlaPolicy{
+	linux.NFTA_NAT_TYPE:          {nlaType: linux.NLA_U32},
+	linux.NFTA_NAT_FAMILY:        {nlaType: linux.NLA_U32},
+	linux.NFTA_NAT_REG_ADDR_MIN:  {nlaType: linux.NLA_U32},
+	linux.NFTA_NAT_REG_ADDR_MAX:  {nlaType: linux.NLA_U32},
+	linux.NFTA_NAT_REG_PROTO_MIN: {nlaType: linux.NLA_U32},
+	linux.NFTA_NAT_REG_PROTO_MAX: {nlaType: linux.NLA_U32},
+	linux.NFTA_NAT_FLAGS:         {nlaType: linux.NLA_BE32, validator: AttrMaskValidator[uint32](linux.NF_NAT_RANGE_MASK)},
+}
+
+// initNATOp initializes a NAT operation from the given expression information.
+// Similar to `nft_nat_init` in kernel.
+func initNATOp(tab *Table, exprInfo ExprInfo) (*natOp, *syserr.AnnotatedError) {
+	attrs, ok := NfParseWithPolicy(exprInfo.ExprData, natAttrPolicy)
+	if !ok {
+		return nil, syserr.NewAnnotatedError(syserr.ErrInvalidArgument, "Nftables: Failed to parse NAT expression data")
+	}
+
+	nt, typeOk := AttrNetToHost[uint32](linux.NFTA_NAT_TYPE, attrs)
+	regAddrMin, regAddrMinOk := AttrNetToHost[uint32](linux.NFTA_NAT_REG_ADDR_MIN, attrs)
+	regProtoMin, regProtoMinOk := AttrNetToHost[uint32](linux.NFTA_NAT_REG_PROTO_MIN, attrs)
+
+	if !typeOk || !(regAddrMinOk || regProtoMinOk) {
+		return nil, syserr.NewAnnotatedError(syserr.ErrInvalidArgument, "Nftables: Failed to parse NAT expression data")
+	}
+
+	family, familyOk := AttrNetToHost[uint32](linux.NFTA_NAT_FAMILY, attrs)
+	if !familyOk {
+		return nil, syserr.NewAnnotatedError(syserr.ErrInvalidArgument, "Nftables: Failed to parse NAT family expression data")
+	}
+	af, err := AFtoNetlinkAF(uint8(family))
+	if err != nil {
+		return nil, syserr.NewAnnotatedError(syserr.ErrInvalidArgument, "Nftables: Failed to convert family to AF")
+	}
+	tf := tab.GetAddressFamily()
+	if tf != stack.Inet && tf != af {
+		return nil, syserr.NewAnnotatedError(syserr.ErrInvalidArgument, "Nftables: Failed to parse NAT family expression data")
+	}
+
+	flags, _ := AttrNetToHost[uint32](linux.NFTA_NAT_FLAGS, attrs)
+	sregAddrMin, sregAddrMax := int8(-1), int8(-1)
+	if regAddrMinOk {
+		sregAddrMin = int8(regAddrMin)
+		regAddrMax, regAddrMaxOk := AttrNetToHost[uint32](linux.NFTA_NAT_REG_ADDR_MAX, attrs)
+		if !regAddrMaxOk {
+			sregAddrMax = sregAddrMin
+		} else {
+			sregAddrMax = int8(regAddrMax)
+		}
+		flags |= linux.NF_NAT_RANGE_MAP_IPS
+	}
+
+	sregProtoMin, sregProtoMax := int8(-1), int8(-1)
+	if regProtoMinOk {
+		sregProtoMin = int8(regProtoMin)
+		regProtoMax, regProtoMaxOk := AttrNetToHost[uint32](linux.NFTA_NAT_REG_PROTO_MAX, attrs)
+		if !regProtoMaxOk {
+			sregProtoMax = sregProtoMin
+		} else {
+			sregProtoMax = int8(regProtoMax)
+		}
+		flags |= linux.NF_NAT_RANGE_PROTO_SPECIFIED
+	}
+
+	return newNATOp(uint8(nt), uint8(family), sregAddrMin, sregAddrMax, sregProtoMin, sregProtoMax, uint16(flags))
+}

pkg/abi/linux/nf_tables.go
@@ -522,3 +522,18 @@ const (
 	__NFTA_GEN_MAX
 	NFTA_GEN_MAX = __NFTA_GEN_MAX - 1
 )
+
+// Nf table nat expression netlink attributes.
+// These correspond to enum values in include/uapi/linux/netfilter/nf_tables.h.
+const (
+	NFTA_NAT_UNSPEC uint16 = iota
+	NFTA_NAT_TYPE
+	NFTA_NAT_FAMILY
+	NFTA_NAT_REG_ADDR_MIN
+	NFTA_NAT_REG_ADDR_MAX
+	NFTA_NAT_REG_PROTO_MIN
+	NFTA_NAT_REG_PROTO_MAX
+	NFTA_NAT_FLAGS
+	__NFTA_NAT_MAX
+	NFTA_NAT_MAX = __NFTA_NAT_MAX - 1
+)

pkg/tcpip/nftables/BUILD
@@ -25,6 +25,7 @@ go_library(
         "nft_meta.go",
         "nft_metaload.go",
         "nft_metaset.go",
+        "nft_nat.go",
         "nft_payload.go",
         "nft_payload_load.go",
         "nft_payload_set.go",

pkg/tcpip/nftables/nftables.go
@@ -1269,6 +1269,10 @@ func (r *Rule) AddOpFromExprInfo(tab *Table, exprInfo ExprInfo) *syserr.Annotate
 		if op, err = initCounter(tab, exprInfo); err != nil {
 			return err
 		}
+	case OpTypeNAT:
+		if op, err = initNATOp(tab, exprInfo); err != nil {
+			return err
+		}
 
 	default:
 		return syserr.NewAnnotatedError(syserr.ErrNoFileOrDir, fmt.Sprintf("Nftables: Unknown expression type not found: %s", exprInfo.ExprName))

pkg/tcpip/nftables/nftables_attrs.go
@@ -128,6 +128,14 @@ func AttrMaxValidator[T integer](maxValue T) NlaPolicyValidator {
 	}
 }
 
+// AttrMaskValidator checks if the data is a subset of the mask.
+func AttrMaskValidator[T integer](mask T) NlaPolicyValidator {
+	return func(data any) bool {
+		v, ok := data.(T)
+		return ok && ((v & ^mask) == 0)
+	}
+}
+
 // NfParseWithPolicy parses the data bytes, clearing the nested attribute bit if present.
 // For nested attributes, Linux supports these attributes having the bit
 // set or unset. It is cleared here for consistency. The policy map is used to validate the

pkg/tcpip/nftables/nftables_types.go
@@ -755,6 +755,8 @@ const (
 	OpTypeByteorder
 	// OpTypeMeta is the meta operation type.
 	OpTypeMeta
+	// OpTypeNAT is the NAT operation type.
+	OpTypeNAT
 	// OpTypeUnknown is the unknown operation type.
 	OpTypeUnknown
 )
@@ -770,6 +772,7 @@ var opTypeStrings = []string{
 	OpTypeRoute:      "route",
 	OpTypeByteorder:  "byteorder",
 	OpTypeMeta:       "meta",
+	OpTypeNAT:        "nat",
 	OpTypeUnknown:    "unknown",
 }
 

pkg/tcpip/stack/nat.go
@@ -22,6 +22,52 @@ import (
 	"gvisor.dev/gvisor/pkg/tcpip/header"
 )
 
+// NATType represents the type of NAT.
+type NATType int
+
+const (
+	// SNAT is source NAT.
+	SNAT NATType = iota
+	// DNAT is destination NAT.
+	DNAT
+	// NATUnknown is unknown NAT type.
+	NATUnknown
+)
+
+// ToNATType converts a uint8 to a NATType.
+func ToNATType(t uint8) NATType {
+	switch t {
+	case 0:
+		return SNAT
+	case 1:
+		return DNAT
+	}
+	return NATUnknown
+}
+
+func (natType NATType) String() string {
+	switch natType {
+	case SNAT:
+		return "SNAT"
+	case DNAT:
+		return "DNAT"
+	default:
+		return "NATUnknown"
+	}
+}
+
+// NfHookToNATType returns the applicable NAT type
+// for the given netfilter hook.
+func NfHookToNATType(hook NFHook) NATType {
+	switch hook {
+	case NFPrerouting, NFOutput:
+		return DNAT
+	case NFInput, NFPostrouting:
+		return SNAT
+	}
+	return NATUnknown
+}
+
 // NfNATPriority returns the priority of the NAT hook.
 // Check `ipv4/ipv6_nat_ops` in nf_nat_proto.c.
 func NfNATPriority(hook NFHook) (int, bool) {

test/syscalls/linux/socket_netlink_netfilter.cc
@@ -3620,6 +3620,84 @@ INSTANTIATE_TEST_SUITE_P(
       return info.param.test_name;
     });
 
+std::vector<RuleWithExprTestParams> GetNATRuleTestParams() {
+  return {
+      RuleWithExprTestParams{
+          .test_name = "SNATAddrPortIPv4",
+          .expr_name = "nat",
+          .expr_attrs = NlNestedAttr()
+                            .U32Attr(NFTA_NAT_TYPE, NFT_NAT_SNAT)
+                            .U32Attr(NFTA_NAT_FAMILY, AF_INET)
+                            .U32Attr(NFTA_NAT_REG_ADDR_MIN, NFT_REG_1)
+                            .U32Attr(NFTA_NAT_REG_PROTO_MIN, NFT_REG_2)},
+      RuleWithExprTestParams{
+          .test_name = "DNATAddrPortIPv4",
+          .expr_name = "nat",
+          .expr_attrs = NlNestedAttr()
+                            .U32Attr(NFTA_NAT_TYPE, NFT_NAT_DNAT)
+                            .U32Attr(NFTA_NAT_FAMILY, AF_INET)
+                            .U32Attr(NFTA_NAT_REG_ADDR_MIN, NFT_REG_1)
+                            .U32Attr(NFTA_NAT_REG_PROTO_MIN, NFT_REG_2)},
+      RuleWithExprTestParams{
+          .test_name = "SNATAddrProtoMinMaxIPv4",
+          .expr_name = "nat",
+          .expr_attrs = NlNestedAttr()
+                            .U32Attr(NFTA_NAT_TYPE, NFT_NAT_SNAT)
+                            .U32Attr(NFTA_NAT_FAMILY, AF_INET)
+                            .U32Attr(NFTA_NAT_REG_ADDR_MIN, NFT_REG_1)
+                            .U32Attr(NFTA_NAT_REG_ADDR_MAX, NFT_REG_4)
+                            .U32Attr(NFTA_NAT_REG_PROTO_MIN, NFT_REG32_00)
+                            .U32Attr(NFTA_NAT_REG_PROTO_MAX, NFT_REG32_15)},
+      RuleWithExprTestParams{
+          .test_name = "DNATAddrProtoMinMaxIPv6",
+          .expr_name = "nat",
+          .expr_attrs = NlNestedAttr()
+                            .U32Attr(NFTA_NAT_TYPE, NFT_NAT_DNAT)
+                            .U32Attr(NFTA_NAT_FAMILY, AF_INET6)
+                            .U32Attr(NFTA_NAT_REG_ADDR_MIN, NFT_REG32_00)
+                            .U32Attr(NFTA_NAT_REG_ADDR_MAX, NFT_REG32_15)
+                            .U32Attr(NFTA_NAT_REG_PROTO_MIN, NFT_REG_1)
+                            .U32Attr(NFTA_NAT_REG_PROTO_MAX, NFT_REG_4)},
+      RuleWithExprTestParams{
+          .test_name = "NoNATType",
+          .expr_name = "nat",
+          .expr_attrs = NlNestedAttr()
+                            .U32Attr(NFTA_NAT_FAMILY, AF_INET)
+                            .U32Attr(NFTA_NAT_REG_ADDR_MIN, NFT_REG_1),
+          .expected_error_no = EINVAL},
+      RuleWithExprTestParams{
+          .test_name = "NoNATFamily",
+          .expr_name = "nat",
+          .expr_attrs = NlNestedAttr()
+                            .U32Attr(NFTA_NAT_TYPE, NFT_NAT_SNAT)
+                            .U32Attr(NFTA_NAT_REG_ADDR_MIN, NFT_REG_1),
+          .expected_error_no = EINVAL},
+      RuleWithExprTestParams{
+          .test_name = "NoAddrOrProto",
+          .expr_name = "nat",
+          .expr_attrs = NlNestedAttr()
+                            .U32Attr(NFTA_NAT_TYPE, NFT_NAT_SNAT)
+                            .U32Attr(NFTA_NAT_FAMILY, AF_INET),
+          .expected_error_no = EINVAL},
+      RuleWithExprTestParams{
+          .test_name = "InvalidNATType",
+          .expr_name = "nat",
+          .expr_attrs = NlNestedAttr()
+                            // NAT type should be either 0 or 1.
+                            .U32Attr(NFTA_NAT_TYPE, 2)
+                            .U32Attr(NFTA_NAT_FAMILY, AF_INET)
+                            .U32Attr(NFTA_NAT_REG_ADDR_MIN, NFT_REG_1),
+          .expected_error_no = EINVAL},
+  };
+}
+
+INSTANTIATE_TEST_SUITE_P(NATRuleTest, AddRuleWithExprTest,
+                         /*param_generator=*/ValuesIn(GetNATRuleTestParams()),
+                         /*param_name_generator=*/
+                         [](const TestParamInfo<RuleWithExprTestParams>& info) {
+                           return info.param.test_name;
+                         });
+
 }  // namespace
 
 }  // namespace testing

commit 771b8890647be1fd79caef4fbe17dd20219ec587

nftables: skip NAT rules whose family does not match the packet's family
In an `inet` table, a single base chain is dispatched for both IPv4 and
IPv6 packets. initNATOp already accepts a NAT rule whose NFTA_NAT_FAMILY
differs from the table family when the table is `inet`
(nft_nat.go:230-233). However, setupNetmap later sizes its iteration
loop by the packet's family while getAddrRange sized the address slices
by the rule's family. When the rule family is NFPROTO_IPV4 (4-byte
addresses) but the packet is IPv6 (16-byte addresses), the loop walks
minSlice[i*4:] past the end of a 4-byte slice and binary.BigEndian.Uint32
panics with "index out of range".

There is no recover() in the netfilter packet-processing path, so the
panic propagates and crashes the sentry. Confirmed live on
runsc release-20260601.0-31-gf028e6a84187-dirty with
--TESTONLY-nftables=true.

Linux's nft_nat_eval (linux/net/netfilter/nft_nat.c) handles the
mismatch by skipping the rule. Mirror that behavior in evaluate().

Fixes: 5d1cd104 ("nftables: Added nat expression support.")
Signed-off-by: moraneus <moraneus@gmail.com>

pkg/tcpip/nftables/nft_nat.go
@@ -154,6 +154,22 @@ func (n *natOp) setupNetmap(pkt *stack.PacketBuffer, minAddr, maxAddr *tcpip.Add
 // evaluate performs NAT setup on the connection.
 // Called when the packet matches the NAT op configured.
 func (n *natOp) evaluate(regs *registerSet, pkt *stack.PacketBuffer, rule *Rule) {
+	// Skip the rule if the packet's family does not match the configured rule
+	// family. With an `inet` table the same base chain is dispatched for both
+	// IPv4 and IPv6 packets, so this mismatch is reachable in practice and
+	// must not panic inside setupNetmap. Matches the behavior of nft_nat_eval
+	// in linux/net/netfilter/nft_nat.c.
+	switch n.family {
+	case linux.NFPROTO_IPV4:
+		if pkt.NetworkProtocolNumber != header.IPv4ProtocolNumber {
+			return
+		}
+	case linux.NFPROTO_IPV6:
+		if pkt.NetworkProtocolNumber != header.IPv6ProtocolNumber {
+			return
+		}
+	}
+
 	// Just fill the data for the NAT operation.
 	changeAddress := false
 	changePort := false